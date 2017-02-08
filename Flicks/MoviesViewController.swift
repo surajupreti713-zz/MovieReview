30b627fb5e7128908da70efb34a2dd1402448f1a
Suraj Upreti <surajupreti@Surajs-MacBook-Pro.local>
2017-02-08 18:45:01 -0500

    adding the project to new repository

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -11,24 +11,35 @@ import AFNetworking
 import MBProgressHUD
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    var searchBar: UISearchBar!
+    
     var movies: [NSDictionary]?
+    var filteredMovies: [NSDictionary]?
+    
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var errorView: UIView!
     
     let refreshControl = UIRefreshControl()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        filteredMovies = movies
         
+        //hiding the keyboard when the user taps anywhere but the search bar
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
         
+        errorView.isHidden = true
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
-        
         tableView.dataSource = self
         tableView.delegate = self     //this will confirm the delegate for the table view
-        
         createSearchBar()
-        
         requestNetwork()
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
     }
     
     func refreshControlAction(_ refreshControl: UIRefreshControl) {
@@ -40,36 +51,44 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        
         MBProgressHUD.showAdded(to: self.tableView, animated: true)    //this will show the loading icon before loading the page
-        
         let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
             MBProgressHUD.hide(for: self.tableView, animated: true)   //this will hide the loading icon after the page is loaded
             if let data = data {
+                self.errorView.isHidden = true
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
                     self.movies = dataDictionary["results"] as! [NSDictionary]
+                    self.filteredMovies = self.movies
                     self.tableView.reloadData()
-                    
                 }
-                
+            } else {
+                self.errorView.isHidden = false
             }
             self.refreshControl.endRefreshing()
         }
         task.resume()
     }
     
-    func createSearchBar() {                 //this function will create a search bar manually
-        let searchBar = UISearchBar()
+    func createSearchBar() {                 //this function will create a search bar programmatically
+        searchBar = UISearchBar()
         searchBar.showsCancelButton = false
         searchBar.placeholder = "Enter your search here"
         searchBar.delegate = self               //confirms the delegate property for the search bar
-        
         self.navigationItem.titleView = searchBar       //this will set the search bar at the navigation bar
+        searchBar.showsCancelButton = true
+        
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        self.filteredMovies = self.movies
+        tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let movies = movies {
-            return movies.count                     //returns the total number of items in movies array
+        if let filteredMovies = filteredMovies {
+            return filteredMovies.count                     //returns the total number of items in movies array
         } else {
             return 0
         }
@@ -77,7 +96,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
-        let movie = movies![indexPath.row]
+        let movie = self.filteredMovies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         let posterPath = movie["poster_path"] as! String
@@ -88,6 +107,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.overviewLabel.text = overview
         cell.posterView.setImageWith(imageUrl as! URL)
         return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        self.filteredMovies = searchText.isEmpty ? movies : movies?.filter({(movie: NSDictionary) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return (movie["title"] as? String)?.range(of: searchText, options: .caseInsensitive) != nil
+        })
+        tableView.reloadData()
     }
     
     
